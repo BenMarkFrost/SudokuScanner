8ceddf44aeed11b4f18bf384beec15e14a48467d
Benjamin Frost <43476782+BenMarkFrost@users.noreply.github.com>
2021-03-28 02:25:33 +0100

    Adding aiortc

---
+++ app_old.py
@@ -0,0 +1,59 @@
+# sys.path.insert(1, '/server')
+from flask import Flask, request, Response, render_template, send_file
+from PIL import Image
+import time
+from server import sudokuscanner
+import codecs, json
+import cv2
+from io import BytesIO
+import numpy as np
+import sys
+sys.path.append('../')
+
+
+# TODO Clean up blobs!!
+
+app = Flask(__name__, 
+            static_url_path='', 
+            static_folder='public',
+            template_folder='public/templates')
+
+# app.config["DEBUG"] = True
+
+@app.route('/')
+def root():
+    return render_template("index.html")
+
+@app.route('/frame', methods=['POST'])
+def frame():
+    try:
+
+        frame_id = request.form.get("id")
+        browser_id = request.form.get("browser_id")
+        frame = Image.open(request.files['frame'])
+
+        # print(browser_id)
+
+        frame = np.array(frame)
+
+        outputImage, calculated = sudokuscanner.scan(frame, browser_id, frame_id)
+
+        # outputImage = np.zeros(frame.shape)
+
+        imgIO = BytesIO()
+        pilImg = Image.fromarray((outputImage).astype(np.uint8))
+        pilImg.save(imgIO, 'JPEG', quality=50)
+        imgIO.seek(0)
+
+        returnFile = send_file(imgIO, mimetype='img/jpeg')
+
+        returnFile.headers["x-filename"] = frame_id
+
+        return returnFile
+
+    except Exception as e:
+        print("Post /frame error: " + str(e))
+        return e
+
+
+app.run(host='0.0.0.0', threaded=True)
